5668c04540dcf642538374ff138eb623f24321f2
Cedric Dumay <cedric.dumay@gmail.com>
2018-08-07 14:55:51 +0200

    feat: crypto feature rewrote

--- src/kser/crypto.py
+++ src/kser/crypto.py
@@ -7,47 +7,62 @@
 
 """
 import base64
+import os
 import csodium
+import marshmallow
+
+from cdumay_error import ValidationError
 from marshmallow import Schema, fields
 from kser.schemas import Message
 
 
-class CryptoMessage(Schema):
+class CryptoSchema(Schema):
     data = fields.String(required=True)
     nonce = fields.String(required=True)
 
-    @classmethod
-    def encode(cls, kmsg, secretbox_key):
+    def encode(self, kmsg):
         """ Encode message using libsodium
 
         :param kser.schemas.Message kmsg: Kafka message
-        :param str secretbox_key: Secrebox Key
         :return: the Encoded message
         """
         nonce = csodium.randombytes(csodium.crypto_box_NONCEBYTES)
-        return cls().dumps(dict(
+        return self.dumps(dict(
             nonce=base64.encodebytes(nonce).strip(),
             data=base64.encodebytes(
                 csodium.crypto_secretbox(
                     bytes(kmsg.MARSHMALLOW_SCHEMA.dumps(kmsg).data, 'utf-8'),
-                    nonce, base64.b64decode(secretbox_key)
+                    nonce, base64.b64decode(self.context['secretbox_key'])
                 )
             ).strip()
         ))
 
-    @classmethod
-    def decode(cls, jdata, secretbox_key):
+    def decode(self, jdata):
         """ Decode message using libsodium
 
         :param str jdata: jdata to load
         :param str secretbox_key: Secrebox Key
         :return: the Encoded message
         """
-        ckmsg = cls().loads(jdata)
+        ckmsg = self.loads(jdata)
         return Message.loads(
             csodium.crypto_secretbox_open(
                 base64.b64decode(ckmsg["data"]),
                 base64.b64decode(ckmsg["nonce"]),
-                base64.b64decode(secretbox_key)
+                base64.b64decode(self.context['secretbox_key'])
             ).decode('utf-8')
         )
+
+
+class CryptoMessage(Message):
+    MARSHMALLOW_SCHEMA = CryptoSchema(
+        context=dict(secretbox_key=os.getenv("KSER_SECRETBOX_KEY", None))
+    )
+
+    @classmethod
+    def loads(cls, json_data):
+        """description of load"""
+        try:
+            return cls(**cls.MARSHMALLOW_SCHEMA.decode(json_data))
+        except marshmallow.exceptions.ValidationError as exc:
+            raise ValidationError("Failed to load message", extra=exc.args[0])
